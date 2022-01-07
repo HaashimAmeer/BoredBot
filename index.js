9ca783009baf69dd6622326b3471c6a37fbacd2b
Haashim Ameer <34925045+HaashimAmeer@users.noreply.github.com>
2022-01-06 17:40:54 -0800

    HTML skeleton

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 fetch("https://www.boredapi.com/api/activity")
   .then((response) => response.json())
   .then((data) => {
-    document.getElementById("activity").innerHTML = `<h1>${data.activity}</h1>`;
+    document.getElementById("activity-name").textContent = `${data.activity}`;
     console.log(data.activity);
   });
