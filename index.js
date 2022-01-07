b520008857f0455e768d9b0758ec5066678a4e35
Haashim Ameer <34925045+HaashimAmeer@users.noreply.github.com>
2022-01-06 18:10:36 -0800

    CSS/JS styling

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-fetch("https://www.boredapi.com/api/activity")
-  .then((response) => response.json())
-  .then((data) => {
-    document.getElementById("activity-name").textContent = `${data.activity}`;
-    console.log(data.activity);
-  });
+let refreshBtn = document.getElementById("refresh-btn");
+refreshBtn.addEventListener("click", function () {
+  fetch("https://www.boredapi.com/api/activity")
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById(
+        "activity-name"
+      ).textContent = `${data.activity}.`;
+      //   console.log(data.activity);
+    });
+});
